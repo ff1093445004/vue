573588140097c6dc35ca799a5c0ae4248fe5c2c8
root <gogs@gogs.com>
2018-11-19 17:29:47 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,21 @@
     <h2>进行中</h2>
     <ul>
       <li v-for="(item, key) in list" v-if="!item.ck">
-        <input type="checkbox" v-model="item.ck">{{item.msg}} ----------------- <button @click="remove(key)">删除</button>
+        <input type="checkbox" v-model="item.ck" @change="changeAdd()">{{item.msg}} ----------------- <button @click="remove(key)">删除</button>
       </li>
     </ul>
     <h2>已完成</h2>
      <ul>
       <li v-for="(item, key) in list" v-if="item.ck">
-        <input type="checkbox" v-model="item.ck">{{item.msg}} ----------------- <button @click="remove(key)">删除</button>
+        <input type="checkbox" v-model="item.ck" @change="changeAdd()">{{item.msg}} ----------------- <button @click="remove(key)">删除</button>
       </li>
     </ul>
+    <hr>
+    <button @click="getData()">傻狗</button>
   </div>
 </template>
 <script>
+import storeage from "./model/storeage.js";
 export default {
   name: "app",
   data() {
@@ -27,16 +30,41 @@ export default {
   },
   methods: {
     addList(e) {
-      if(e.keyCode == 13){
-          this.list.push({
-          ck:false,
-          msg:this.todo
+      if (e.keyCode == 13) {
+        this.list.push({
+          ck: false,
+          msg: this.todo
         });
         this.todo = "";
       }
+      storeage.set("list", this.list);
     },
     remove(key) {
       this.list.splice(key, 1);
+      storeage.set("list", this.list);
+    },
+    changeAdd() {
+      storeage.set("list", this.list);
+    },
+    getData() {
+      this.$http
+        .get(
+          "https://www.baidu.com/home/msg/data/personalcontent?callback=jQuery1102003859572038479919_1542619485984&num=8&_req_seqid=86d1460f000303b5&sid=&_=1542619485985"
+        )
+        .then(
+          function(response) {
+            console.info(response);
+          },
+          function(error) {
+            console.info(error);
+          }
+        );
+    }
+  },
+  mounted() {
+    var list1 = storeage.get("list");
+    if (list1) {
+      this.list = list1;
     }
   }
 };
